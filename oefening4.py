9dfffa365ac3e53905036c11c8425b7bbc3875d8
Lenthe Van Cleemput <lenthe09@gmail.com>
2025-03-25 09:20:01 +0100

    oefening 4

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,11 @@
-def countTargetPairs():
+def TelDoelParen():
+    n = len(nummers)
+    count = 0
+    for getal in range(n):
+        for getal2 in range(getal + 1):
+            if nums[getal] + nums[getal2] < doel :
+                tel += 1
+    return tel
+nummers = [1 ,2, 3, 4, 5]
+doel = 6
+print(TelDoelParen(nummers, doel))
